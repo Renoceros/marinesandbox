7a0402e74f934ed8b968a861f9512bd30ecb1b5d
Renoceros <130336017+Renoceros@users.noreply.github.com>
2026-08-11 08:35:31 +0800

    Initial Commit

---
+++ marinesandbox/marinesandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  marinesandboxApp.swift
+//  marinesandbox
+//
+//  Created by Moreno Kristovan on 11/08/26.
+//
+
+import SwiftUI
+
+@main
+struct marinesandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

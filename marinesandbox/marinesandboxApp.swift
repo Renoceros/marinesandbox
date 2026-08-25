8fa70edc10c5580aba2e3d1d7ce59ff66bed684c
Samantha Joice <kyj.samantha@gmail.com>
2026-08-25 16:08:34 +0800

    fix: onboarding & splash screen

--- marinesandbox/marinesandboxApp.swift
+++ marinesandbox/marinesandboxApp.swift
@@ -3,9 +3,23 @@ import SwiftData
 
 @main
 struct marinesandboxApp: App {
+    @State private var isSplashScreenActive: Bool = true
     var body: some Scene {
+        
         WindowGroup {
-            RootView()
+            if isSplashScreenActive {
+                SplashScreenView()
+                    .onAppear {
+                        // splash view active for 7 seconds, then transition
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 7.0) {
+                            withAnimation(.easeOut(duration: 0.4)) {
+                                isSplashScreenActive = false
+                            }
+                        }
+                    }
+            } else {
+                RootView()
+            }
         }
         .modelContainer(for: [UserProfile.self, ReefCanvas.self, CoralFrag.self, NGOConfig.self])
     }
